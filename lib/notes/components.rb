8d1b4a33f6c8ebf610c0c8ed8765a68e460b2637
Alex Musayev <alex.musayev@gmail.com>
2023-08-20 19:35:08 +0200

    Use short timestamps for the page lists

--- lib/notes/components.rb
+++ lib/notes/components.rb
@@ -1,5 +1,7 @@
 class Notes::Components
   class << self
+    include DOTIW::Methods
+
     def tags_list(tags:, current_tag: nil)
       tags.map { tag(tag: _1, highlight: _1 == current_tag) }.join
     end
@@ -7,27 +9,33 @@ class Notes::Components
     def pages_list(pages)
       return if pages.none?
 
-      <<~HTML.strip
-        <ul>
-          <% pages.each do |page| %>
-            <li>
-              <a href="<%= page.public_path %>"><%= page.title %></a>
-              <small><%= timestamp(page) %></small>
-            </li>
-          <% end %>
-        </ul>
-      HTML
+      pages.map do |page|
+        <<~HTML.strip
+          <li>
+            <a href="#{page.public_path}">#{page.title}</a>
+            <small class="text-slate-400">#{ago_timestamp(page)}</small>
+          </li>
+        HTML
+      end.then { "<ul>#{_1.join}</ul>" }
     end
 
-    def timestamp(page)
-      page.published_at&.then do |published_at|
-        <<~HTML.strip
-          <time datetime="#{published_at.to_time}">#{published_at.strftime("%Y/%m/%d")}</time>
-        HTML
-      end
+    def ago_timestamp(page)
+      page.published_at&.then { time_tag(time: _1, label: ago(_1)) }
+    end
+
+    def page_timestamp(page)
+      page.published_at&.then { time_tag(time: _1, label: _1.strftime("%Y/%m/%d")) }
     end
 
     private
+
+    def time_tag(time:, label:)
+      "<time datetime=\"#{time.to_time}\">#{label}</time>"
+    end
+
+    def ago(time)
+      distance_of_time_in_words(time, Time.now, compact: true, highest_measure_only: true)
+    end
 
     def tag(tag:, highlight: false)
       classes = %w[block border rounded-md py-0 px-2 my-1 mr-2 no-underline font-normal]
